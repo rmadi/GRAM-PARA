187d0b887b3186338cd334aa2ded19af7d9fb3a3
Rihem <rihambensaad@gmail.com>
2021-04-02 13:47:10 +0100

    fix rihem

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const routes = require('./database/routers/index.js')
 app.use(express.json());
 app.use(express.static(__dirname + '/../client/dist/gram-para'));
 
-app.use('/product',routes.product );
+app.use('/product',routes.product);
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
