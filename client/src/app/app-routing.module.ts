0432d848125f0f1025111a768f7149931e48cc49
Rmadi Med <rmadi.med1@gmail.com>
2021-04-05 17:29:39 +0100

    add a form for registration

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:NavbarComponent},
+  {path: 'register', component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
