2229a18a9543d48e0c34662a6fdb7fa972142c4b
Rihem <rihambensaad@gmail.com>
2021-04-08 03:28:26 +0100

    med

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
 
